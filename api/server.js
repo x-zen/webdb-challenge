40d65f1d64289fd440102c8aaa1ffa457ae32ae0
x-zen <jdharris.x@gmail.com>
2019-05-03 11:11:49 -0500

    Server live

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const projectRouter = require('./projects/projectRouter.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'You are here...' });
+});
+
+module.exports = server;
